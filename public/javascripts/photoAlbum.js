487b8cc16e21db21dc9b4619910429e0c7f28cb2
gyj <505324327@qq.com>
2014-07-10 01:21:26 +0800

    已优化，仍有些BUG

--- public/javascripts/photoAlbum.js
+++ public/javascripts/photoAlbum.js
@@ -5,6 +5,7 @@ function photoAlbum(opts){
 	this._container=document.getElementById(opts.containerId);
 	this._photos=opts.list;
 	this._style=opts.style;
+	this._size=opts.size||100;
 	//构造三部曲
 	this.init();
 	this.renderDOM();
@@ -13,27 +14,23 @@ function photoAlbum(opts){
 
 photoAlbum.prototype.init=function()
 {
+	this._winW=window.innerWidth;
+	this._winH=window.innerHeight;
 	this._breviary=document.createElement("div");
 	this._breviary.id = "breviary";
 	this._large=document.createElement("div");
 	this._large.id = "large";
+	this._large.style.display = "none";
+	this._large.setAttribute("class","animated fadeIn");
+	this._largeCvs=document.createElement("canvas");
+	this._largeCvs.width=this._winW;
+	this._largeCvs.height=this._winH-2;
+	this._largeCvsContext=this._largeCvs.getContext("2d");
+	this._large.appendChild(this._largeCvs);
 	this._container.appendChild(this._large);
-
-	this._winW=window.innerWidth;
-	this._winH=window.innerHeight;
 	this._breviaryUl=document.createElement("ul");
 	this._wholeCount=this._photos.length;
 	this._marginBottom="5px";
-
-	switch (this._style){
-		case "ratio":
-			this._lisInOneLine=[];
-			this._lisInOneLineWidth=0;
-			break;
-		case "square":
-			break;
-		default :break;
-	}
 };
 
 photoAlbum.prototype.renderDOM=function(){
@@ -42,217 +39,133 @@ photoAlbum.prototype.renderDOM=function(){
 
 	for(var i=0;i<parent._wholeCount;i++){
 		var img=new Image();
-
 		img.onload = function(){
+			var tmpcvs=document.createElement("canvas");
+			tmpcvs.id = curphoto;
+			tmpcvs.setAttribute("data-src",this.src);
+			tmpcvs.setAttribute("data-width",this.width);
+			tmpcvs.setAttribute("data-height",this.height);
+			tmpcvs.width=parent._size;
+			tmpcvs.height=parent._size;
 			this._li=document.createElement("li");
-			this._img=document.createElement("img");
-			this._img.setAttribute("src",this.src);
-			this._img.id = curphoto;
+			this._li.appendChild(tmpcvs);
+			this._cvs=tmpcvs.getContext("2d");
 			curphoto+=1;
-			switch (parent._style){
-				case "ratio":
-					parent.renderImgs_ratio(parent, this, curphoto);
-					break;
-				case "square":
-					parent.renderImgs_square(parent, this, curphoto);
-					break;
-			}
+			parent.renderImgs_square(parent, this, curphoto);
 		};
-
 		img.src = this._photos[i].src;
 	}
 	parent._breviary.appendChild(parent._breviaryUl);
 	parent._container.appendChild(parent._breviary);
 };
 
-photoAlbum.prototype.renderImgs_ratio=function(parent,_this,_curphoto){
-	var thisLiW=0           //	    目前照片宽度
-		,ratio_style="";    //	    横版还是竖版？
-
-//	如果图片宽度比高度大20%，则认为这张图是横版, 否则认为是竖版
-	if(_this.width/_this.height>1.2){
-//		横版   (缩略图尺寸 130[宽]*100[高])
-		ratio_style="horizontal";
-		thisLiW=130;
-	}else{
-//		竖版   (缩略图尺寸 80[宽]*100[高])
-		ratio_style="vertical";
-		thisLiW=80;
-	}
-	_this._img.setAttribute("class",ratio_style);
-	_this._li.appendChild(_this._img);
-	_this._li.setAttribute("class",ratio_style+" animated bounceIn");
-	if(parent._lisInOneLineWidth+thisLiW<=parent._winW){
-		parent._lisInOneLineWidth+=thisLiW;
-		parent._lisInOneLine.push(_this._li);
-	}else{
-		parent.renderPaddingOfLi();
-		parent._lisInOneLine.length=0;
-		parent._lisInOneLine.push(_this._li);
-		parent._lisInOneLineWidth=thisLiW;
-	}
-//	如果处理的照片是最后一个，要再次调用renderPaddingOfLi以确保当前暂存列表中已经完全处理完
-	if(_curphoto==parent._wholeCount){
-		parent.renderPaddingOfLi();
-	}
-};
-
-photoAlbum.prototype.renderImgs_square=function(parent,_this,_curphoto){
-	var thisLiW=100         //	目前照片宽度
+photoAlbum.prototype.renderImgs_square=function(parent,thisPhoto,curphotoNo){
+	var thisLiW=parent._size         //	目前照片宽度
 		,minPadding=0       //	图片相隔最小间隙（padding）
+		,marginTop=parent._marginBottom
 		,numOfLine=Math.floor(parent._winW/(thisLiW+minPadding))  //	一行能放的图个数
 		,paddingLeft=(parent._winW-numOfLine*thisLiW)/numOfLine   //	实际图片相隔间隙
-		,square_style="";   //	横版还是竖版？
+		,zoom_size=thisLiW
+		,zoom_width=0
+		,zoom_height=0;
 
 //	如果图片宽度比高度大20%，则认为这张图是横版, 否则认为是竖版
-	if(_this.width/_this.height>1.2){
-//		横版图 方块风格   (缩略图尺寸 100[宽]*100[高] 高显示完全，以填充满整个方块)
-		square_style="square_horizontal";
+	if(thisPhoto.width/thisPhoto.height>1.2){
+//		横版图 方块风格   (缩略图默认尺寸 100[宽]*100[高] 高显示完全，以填充满整个方块)
+		zoom_height=zoom_size;
+		zoom_width=thisPhoto.width/thisPhoto.height*zoom_height;
 	}else{
-//		竖版图 方块风格   (缩略图尺寸 100[宽]*100[高] 宽显示完全，以填充满整个方块)
-		square_style="square_vertical";
+//		竖版图 方块风格   (缩略图默认尺寸 100[宽]*100[高] 宽显示完全，以填充满整个方块)
+		zoom_width=zoom_size;
+		zoom_height=thisPhoto.height/thisPhoto.width*zoom_width;
 	}
-//	_this._img.className=square_style+",zoom";
-	_this._img.setAttribute("class",square_style);
-	_this._li.appendChild(_this._img);
-	_this._li.setAttribute("class","square animated bounceIn");
-
+	thisPhoto._cvs.drawImage(thisPhoto,0,0,zoom_width,zoom_height);
+	thisPhoto._li.style.width=zoom_size+"px";
+	thisPhoto._li.style.height=zoom_size+"px";
 //	如果是每行第一个图片就不加paddingLeft 不然就要加
-	if(_curphoto%numOfLine!=1){
-		_this._li.style.paddingLeft=paddingLeft+"px";
+	if(curphotoNo%numOfLine!=1){
+		thisPhoto._li.style.paddingLeft=paddingLeft+"px";
 	}
-	_this._li.style.marginTop=paddingLeft+"px";
-	parent._breviaryUl.appendChild(_this._li);
-};
-
-photoAlbum.prototype.renderPaddingOfLi=function(){
-	var numOfLine=this._lisInOneLine.length  //	一行能放的图个数
-		,paddingLeft=0;    //	图片相隔间隙
-
-	if(numOfLine>1){
-		paddingLeft=Math.floor((this._winW-this._lisInOneLineWidth)/numOfLine);
-	}
-	for(var i=0;i<numOfLine;i++){
-		var _thisLi=this._lisInOneLine[i];
-		if(i!=0){
-			_thisLi.style.paddingLeft=paddingLeft+"px";
-		}
-		_thisLi.style.marginTop=this._marginBottom;
-		this._breviaryUl.appendChild(_thisLi);
-	}
+	thisPhoto._li.style.marginTop=marginTop;
+	thisPhoto._li.setAttribute("class","square animated bounceIn");
+	parent._breviaryUl.appendChild(thisPhoto._li);
 };
 
 //	        direction是显示图像来的方向
-photoAlbum.prototype.loadImg=function(parent,_curImgId,direction){
-	var curImg=document.getElementById(_curImgId)
+photoAlbum.prototype.loadImg=function(parent,_curImgId,action,callback){
+	var curImgCvs=document.getElementById(_curImgId)
+		,ratio_img=curImgCvs.getAttribute("data-height")/curImgCvs.getAttribute("data-width")
+		,ratio_win=parent._winH/parent._winW
 		,largeImg=new Image()
-		,largeChildren=parent._large.childNodes;
+		,paddingTop=0
+		,paddingLeft=0
+		,realH=0
+		,realW=0;
 
 	largeImg.onload=function(){
-		var ratio_img=curImg.height/curImg.width
-			,ratio_win=parent._winH/parent._winW
-			,imgEffect="animated "
-			,img=document.createElement("img");
-		img.src = curImg.src;
-		if(direction){
-			switch (direction){
-				case "left":
-					imgEffect+="bounceInLeft";
-					break;
-				case "right":
-					imgEffect+="bounceInRight";
-					break;
-				default :
-					imgEffect+="bounceIn";
-					break;
-			}
+		switch (action){
+			case "swipeLeft":
+				parent._largeCvs.setAttribute("class","animated bounceInRight");
+				break;
+			case "swipeRight":
+				parent._largeCvs.setAttribute("class","animated bounceInLeft");
+				break;
+			default :break;
 		}
+		parent._largeCvsContext.fillStyle="1F364A";//白色为例子；
+		parent._largeCvsContext.fillRect(0,0,parent._winW,parent._winH);
 //		ratio_img<=ratio_win则认为这是一张横版
 		if(ratio_img<=ratio_win){
-			img.width=parent._winW;
-			var realH=ratio_img*img.width     //算出图片真实高度
-				,panddingTop=parseInt((parent._winH-realH)/2);  //算出图片上方留白
-			img.style.paddingTop=panddingTop+"px";
+			realW=parent._winW;
+			realH=ratio_img*realW;     //算出图片真实高度
+			paddingTop=parseInt((parent._winH-realH)/2);  //算出图片上方留白
+			parent._largeCvsContext.drawImage(this,0,paddingTop,realW,realH);
 		}
 //		ratio_img>ratio_win则认为这是一张竖版
 		else if(ratio_img>ratio_win){
-			img.height=parent._winH;
-			var realW=img.height/ratio_img     //算出图片真实宽度
-				,paddingLeft=parseInt((parent._winW-realW)/2);  //算出图片左边留白
-			img.style.paddingLeft=paddingLeft+"px";
+			realH=parent._winH;
+			realW=realH/ratio_img;     //算出图片真实宽度
+			paddingLeft=parseInt((parent._winW-realW)/2);  //算出图片左边留白
+			parent._largeCvsContext.drawImage(this,paddingLeft,0,realW,realH);
 		}
-		if(largeChildren.length>0){
-			parent._large.removeChild(largeChildren[0]);
-		}
-		img.setAttribute("class",imgEffect);
-		parent._large.appendChild(img);
+		callback&&callback();
 	};
-
-	largeImg.src = curImg.src;
+	largeImg.src = curImgCvs.getAttribute("data-src");
 };
 
 photoAlbum.prototype.bindDOM=function()
 {
 	var parent=this
-		,curImgId={};
+		,curImgId=0;
 
-	$(this._breviary).on("tap","img",function(){
+	$(this._breviary).on("tap","canvas",function(){
 		curImgId=this.id;
-		parent._large.style.opacity=0;
 		parent._large.style.display="block";
-//		渐显
-		fade(parent._large,"show");
 		parent.loadImg(parent,curImgId);
 	});
 	$(this._large).on("tap",function(){
-//		渐隐
-		fade(parent._large,"hiden",function(){
-			parent._large.style.display="none";
-		});
+		parent._large.style.display="none";
 	}).on("swipeRight",function(){
         var lastImgId=curImgId-1;
         if(lastImgId>=0){
-	        parent.loadImg(parent,lastImgId,"left");
-	        curImgId=lastImgId;
+	        parent.loadImg(parent,lastImgId,"swipeRight",function(){
+		        parent._largeCvs.addEventListener("webkitAnimationEnd",function(){
+			        parent._largeCvs.removeAttribute("class");
+			        parent._largeCvs.removeEventListener("webkitAnimationEnd");
+		        },false);
+		        curImgId=lastImgId;
+	        });
 		}
     }).on("swipeLeft",function(){
         var nextImgId=parseInt(curImgId)+1;
         if(nextImgId<parent._wholeCount){
-            parent.loadImg(parent,nextImgId,"right");
-	        curImgId=nextImgId;
+            parent.loadImg(parent,nextImgId,"swipeLeft",function(){
+	            parent._largeCvs.addEventListener("webkitAnimationEnd",function(){
+		            parent._largeCvs.removeAttribute("class");
+		            parent._largeCvs.removeEventListener("webkitAnimationEnd");
+	            },false);
+	            curImgId=nextImgId;
+            });
         }
     });
 };
-
-//渐显渐隐函数
-function fade(obj,method,callback){ //method有两个值show或hiden   callback函数为渐显渐隐结束后的回调函数，若无可省
-	var n = (method == "show") ? 0 : 100,
-		ie = (window.ActiveXObject) ? true : false;
-	var time = setInterval(function(){
-		if(method == "show"){
-			if(n < 100){
-				n+=10;
-				if(ie){
-					obj.style.cssText = "filter:alpha(opacity="+n+")";
-				}else{
-					(n==100) ? obj.style.opacity = 1 : obj.style.opacity = "0."+n;
-				}
-			}else{
-				clearTimeout(time);
-				callback&&callback();
-			}
-		}else{
-			if(n > 0){
-				n-=10;
-				if(ie){
-					obj.style.cssText = "filter:alpha(opacity="+n+")";
-				}else{
-					obj.style.opacity = "0."+n;
-				}
-			}else{
-				clearTimeout(time);
-				callback&&callback();
-			}
-		}
-	},30);
-}
